0a586a1a468a0d4f128a2f6b47d714d0e50876a8
Your Name <kawaiineko55@gmail.com>
2024-05-14 23:50:47 +0100

    Project intialisation

--- src/MainPage1.js
+++ src/MainPage1.js
@@ -7,11 +7,13 @@ const MainPage = () => {
             <h1 className={styles.header}>WELCOME TO THE HARRY POTTER WORLD</h1>
             <button className={styles.showCharactersBtn}>Show all characters</button>
             <div className={styles.charactersPopUp}>
-            <img src="./images/students.png" alt="Students"/>
+            {/* <img src="./images/students.png" alt="Students"/> */}
+            <div id="div1" className={styles.d1}></div>
             <button>Hogwarts' students</button>
-            <img src="./images/tutors.png" alt="Tutors"/>
+            {/* <img src="tutors.png" alt="Tutors"/> */}
+            <div id="div2" className={styles.d2}></div>
             <button>Hogwarts' tutors</button>
-            <img src=""alt="Characters"/>
+            {/* <img src=""alt="Characters"/> */}
             <button>Characters from each house</button>
         </div>
         </div>

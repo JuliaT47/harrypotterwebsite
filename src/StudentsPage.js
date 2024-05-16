00493785bbd416d91c511a67ff124d9e907a71ca
Your Name <kawaiineko55@gmail.com>
2024-05-16 19:39:46 +0100

    Student Section

--- src/StudentsPage.js
+++ src/StudentsPage.js
@@ -6,33 +6,120 @@ const StudentsPage = () => {
       <div className={styles.backgroundStudents}>
         <h1 className={styles.h1Students}>HOGWARTS' STUDENTS</h1>
       </div>
-      <div className={styles.studentsDiv}>
-        <div className={styles.potter}>
+      <div className={styles.studentsDiv1}>
+        <div className={`${styles.potter} ${styles.studentCard}`}>
           <h3>Harry Potter</h3>
-          <p>The Boy Who Lived Gryffindor 31-07-1980</p>
-          <p>More information</p>
+          <p>
+            The Boy Who Lived
+            <br /> Gryffindor
+            <br />
+            31-07-1980
+          </p>
+          <p className={styles.moreInfo}>
+            More information{' '}
+            <button className={styles.svgBtn}>
+              <div className={styles.svgContainer}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 55 32"
+                  width="28"
+                  height="16"
+                  className={styles.arrowSvg}
+                >
+                  <path
+                    fill="none"
+                    stroke="#fff"
+                    strokeWidth="3.429"
+                    d="M36.571 0c0 1.691 1.669 4.229 3.36 6.354 2.171 2.743 4.777 5.12 7.771 6.949 2.24 1.371 4.96 2.697 7.154 2.697m.001 0c-2.194 0-4.914 1.303-7.154 2.674-2.994 1.829-5.6 4.229-7.771 6.949-1.691 2.149-3.36 4.686-3.36 6.377m18.285-16H0"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  width="16"
+                  height="16"
+                  className={styles.circleSvg}
+                >
+                  <path
+                    fill="#f9b50c"
+                    d="M32 16c0 8.837-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0s16 7.163 16 16z"
+                  />
+                </svg>
+              </div>
+            </button>
+          </p>
+        </div>
+        <div className={`${styles.granger} ${styles.studentCard}`}>
+          <h3>Hermione Granger</h3>
+          <p>
+            Know-it-all <br /> Gryffindor <br />
+            19-09-1979
+          </p>
+          <p className={styles.moreInfo}>More information</p>
           <button></button>
         </div>
-        <div className={styles.granger}>
-          <h3>Hermione Granger</h3>
+        <div className={`${styles.weasley} ${styles.studentCard}`}>
+          <h3>Ron Weasley</h3>{' '}
+          <p>
+            Dragomir Despard <br />
+            Gryffindor <br />
+            01-03-1980
+          </p>
+          <p className={styles.moreInfo}>More information</p>
+          <button></button>
         </div>
-        <div className={styles.weasley}>
-          <h3>Ron Weasley</h3>
+        <div className={`${styles.malfoy} ${styles.studentCard}`}>
+          <h3>Draco Malfoy</h3>{' '}
+          <p>
+            Pure-blood <br />
+            Slytherin <br />
+            05-06-1980
+          </p>
+          <p className={styles.moreInfo}>More information</p>
+          <button></button>
         </div>
-        <div className={styles.malfoy}>
-          <h3>Draco Malfoy</h3>
+      </div>
+      <div className={styles.studentsDiv2}>
+        <div className={`${styles.diggory} ${styles.studentCard}`}>
+          <h3>Cedric Diggory</h3>{' '}
+          <p>
+            Ced <br />
+            Hufflepuff <br />
+            31-07-1980
+          </p>
+          <p className={styles.moreInfo}>More information</p>
+          <button></button>
         </div>
-        <div className={styles.diggory}>
-          <h3>Cedric Diggory</h3>
+        <div className={`${styles.chang} ${styles.studentCard}`}>
+          <h3>Cho Chang</h3>{' '}
+          <p>
+            Seeker <br />
+            Ravenclaw <br />
+            07-04-1979
+          </p>
+          <p className={styles.moreInfo}>More information</p>
+          <button></button>
         </div>
-        <div className={styles.chang}>
-          <h3>Cho Chang</h3>
+        <div className={`${styles.longbottom} ${styles.studentCard}`}>
+          <h3>Neville Longbottom</h3>{' '}
+          <p>
+            {' '}
+            Head of Gryffindor House <br />
+            Gryffindor <br />
+            30-07-1980
+          </p>
+          <p className={styles.moreInfo}>More information</p>
+          <button></button>
         </div>
-        <div className={styles.longbottom}>
-          <h3>Neville Longbottom</h3>
-        </div>
-        <div className={styles.lovegood}>
-          <h3>Luna Lovegood</h3>
+        <div className={`${styles.lovegood} ${styles.studentCard}`}>
+          <h3>Luna Lovegood</h3>{' '}
+          <p>
+            Née Lovegood <br />
+            Ravenclaw <br />
+            13-02-1981
+          </p>
+          <p className={styles.moreInfo}>More information</p>
+          <button></button>
         </div>
       </div>
     </>
